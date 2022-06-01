314449eda8ed7d64e3f6f16dd98788bbd242459f
Aleksandr Bykov <bykov.alexnder@gmail.com>
2022-06-01 21:52:44 +0400

    initial commit

---
+++ src/main/java/com/aleksB/gifrategenerator/GifRateGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.aleksB.gifrategenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifRateGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifRateGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksB/gifrategenerator/GifRateGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksB.gifrategenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifRateGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
